c2ca1a0636f37d834917d3648c2ef3e363e81917
Cake <mcgrail.shauna@gmail.com>
2019-12-11 12:13:45 -0700

    troubleshooting score text

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
-//defining variables like a boss
 const question = document.getElementById("question");
 const options = Array.from(document.getElementsByClassName("option-text"));
 const scoreText = document.getElementById('score');
+
 
 const timeText = document.getElementById("finalCountdown");
 
@@ -9,9 +9,8 @@ let secondsLeft = 75;
 const scoreNerf = 15;
 
 let currentQuestion = {};
-let score = 0; // score counter
-let availableQuestions = []; //define empty array of available questions. it is a copy of the full question set array
-//this is so I can take questions out of the availableQuestions array as we use them so that it is a new question every time.
+let score = 0;
+let availableQuestions = []; 
 
 let questions = [
     {//each question wil be an object consisting of the following:
@@ -39,7 +38,7 @@ let questions = [
         "answer": 4
     },
     {
-        "question": "Question 4 - the answer is 4",
+        "question": "Placeholder - the answer is 4",
         "option1": "answer 1",
         "option2": "answer 2",
         "option3": "answer 3",
@@ -47,7 +46,7 @@ let questions = [
         "answer": 4
     },
     {
-        "question": "Question 5 - the answer is 2",
+        "question": "Placeholder - the answer is 2",
         "option1": "answer 1",
         "option2": "answer 2",
         "option3": "answer 3",
@@ -59,38 +58,20 @@ let questions = [
 startGame = () => {
     availableQuestions = [...questions]; //the three dots is a spread operator.
     //it takes the [questions] array and spreads it out into a new array. Now called availableQuestions.
-    //creating a separate array so that I can remove questions as they are used so that the same question is not repeated multiple times in one instance of the quiz. 
-    //console.log(availableQuestions); //so... that's the new array created using the spread operator. cool.
-
     setTime(); //call function that starts timer
     getNewQuestion();
-
-
 };
 
-//function to get a new question... obviously.
+//function to get a new question.
 getNewQuestion = () => {
-    //if statement that stops the quiz if all of the questions have been answered.
-    if (availableQuestions.length === 0) {
-        //if above conditions are met, user is taken to the end page.
-        return window.location.assign("end.html");
-    }
+   
     //randomize a question from the array. availableQuestions.length will run through all questions in array
     const questionList = Math.floor(Math.random() * availableQuestions.length);
     //questionList is a list of the current available questions, this will not include questions already used. 
-
-    //to get the current question from the array
-    //currentQuestion is the question that is currently displaying in the quiz
-    //it will pull a question from the questionList array.
     currentQuestion = availableQuestions[questionList];
 
-    //innerText is almost the same as textContent, it sets or returns the text of a specific element.
-    //in this case I am setting the innerText of question in the HTML <h2> tag on the main page. 
-    //it is displaying the currentQuestion defined in the line above. 
     question.innerText = currentQuestion.question;
 
-    //Basically the same thing is happening with the options
-    //grab options, forEach will refercne each option in the array, then 
     options.forEach(option => {
         //this is how to access the custom dataset attribute in the API, we are extracting the number from it
         const number = option.dataset["number"];
@@ -101,26 +82,21 @@ getNewQuestion = () => {
         option.innerText = currentQuestion["option" + number];
     });
 
-    //this will splice the just-used question from our availableQuestions array
-    //this is why we made a copy of the questions array, so that we wont be splicing the question from the master array
-    //the number 1 tells it what question to cut
     availableQuestions.splice(questionList, 1);
 
-}; //this is the end of the getNEwQuestion function
+};
 
-//adding Event Listeners
+//Event Listeners
 options.forEach(option => {
     option.addEventListener("click", e => {
         const selectedOption = e.target;
         const selectedAnswer = selectedOption.dataset["number"];
 
-        let classCorrect = 'correct'; // set the default value to incorrect
+        let classCorrect = 'correct';
         if (selectedAnswer != currentQuestion.answer) {
-            classCorrect = 'incorrect'; // if the selectedAnswer is equal to the currentQuestion.answer then the 'correct' class is applied
+            classCorrect = 'incorrect'; 
         };
 
-        //the selectedoption is the text that is selected, the pareentElement would be the container box that is selected. 
-        //classList.add is adding the classCorrect class to the container element, this will set the class to apply to correct or incorrect
         selectedOption.parentElement.classList.add(classCorrect);
         console.log(classCorrect);
 
@@ -128,12 +104,11 @@ options.forEach(option => {
             secondsLeft -= scoreNerf;
         }
 
-
-        setTimeout(() => { //setTimeout is built into Javascript. I am using it here to create a delay between questions so that a color can be applied signifying correct ot incorrect
-            selectedOption.parentElement.classList.remove(classCorrect);//this is the removal of the class from the container element.
+        //delay set so that green or red coloring will show if correct/incorrect
+    setTimeout(() => { 
+            selectedOption.parentElement.classList.remove(classCorrect);
             getNewQuestion();
-        }, 150); //200 is the parameter of how long the setTimeout delay is (1000 = 1 second)
-        //I set the timer to 150 so that the delay is quick due to the timer.
+        }, 100);
     });
 
 });
@@ -145,14 +120,12 @@ setTime = () => {
 
         if (secondsLeft === 0 || availableQuestions.length === 0) {
             clearInterval(timerInterval);
-            return window.location.pathname("end.html");
-            
+            return window.location.assign("end.html");
             
         }
     }, 1000);
 };
-
-
+scoreText = timerInterval;
 
 
 
